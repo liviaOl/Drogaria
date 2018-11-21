0f67e939efbab6cc8ccd347e1ad9af5710b61527
ThaisEno <45150689+ThaisEno@users.noreply.github.com>
2018-11-21 13:08:37 -0400

    terceira versão

--- Compra.java
+++ Compra.java
@@ -1,10 +1,19 @@
 package compra;
+import java.util.Scanner;
 
-public class Compra {
+public class CompraCerta {
 
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        try{
         Produto produto1 = null;
         produto1.cadastrarProduto("Shampoo", 2, 12.45);
-
+        Remedio remedio1=null;
+        remedio1.cadastrarRemedio("Naridrin", 3, 12.45);
+            System.out.println("BUNDa");
+        }catch(Exception e){
+        Produto produto1=new Produto("Shampoo", 2, 12.45); 
+            System.out.println("CU");
+        }
     }
 }
